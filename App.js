f17b3f043293cbcfec591d3a34b8915926c4ad1f
anonreclaimer <marquel2@live.com>
2021-09-03 21:21:08 -0400

    React Native Project Setup
    Added the React Native project with blank template

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * 
+ *
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello, world!</Text>
+    </View>
+  )
+}
+export default HelloWorldApp;
