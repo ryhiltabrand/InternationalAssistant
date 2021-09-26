4c00193b1189e2572af63a9fd7b2e7e60b9d7691
cs_mresu001 <mresu001@odu.edu>
2021-09-25 21:44:20 -0400

    Create firebase authencatiton with env varibles

---
+++ app/utilities/firebaseAuth.js
@@ -0,0 +1,18 @@
+// Initialize Firebase
+export const credentials = {
+    //Information can be define in CI varibles. Then sed in by the gitlab runner
+    apiKey: "$APIKey",
+  
+    authDomain: "$authDomain",
+  
+    projectId: "$projectId",
+  
+    storageBucket: "$storageBucket",
+  
+    messagingSenderId: "$messagingSenderId",
+  
+    appId: "$appId",
+  
+    measurementId: "$measurementId"
+  
+  };
