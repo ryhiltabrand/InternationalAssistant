730ded4c70d27da974fd82f19e16fd6320c74cd8
cs_mresu001 <mresu001@odu.edu>
2021-09-25 21:02:23 -0400

    modified language, add country, bio, and profile pic

--- __tests__/utilities/database/unit.js
+++ __tests__/utilities/database/unit.js
@@ -7,7 +7,10 @@ function createTestUserInfo() {
     testUser.setName('test')
     testUser.setDateofBirth("2/22/2222")
     testUser.setEmail('root@cs.odu.edu')
-    testUser.setLanguage('English')
+    testUser.setLanguage(['English', 'Spanish'])
+    testUser.setCountry('usa')
+    testUser.setBio('I am groot')
+    testUser.profilePicture('https://res.cloudinary.com/teepublic/image/private/s--rh264MCI--/t_Preview/b_rgb:484849,c_limit,f_jpg,h_630,q_90,w_630/v1517893785/production/designs/2341977_3.jpg')
     
     testUser.setEvents('Something in the Water')
     testUser.setFriends(['Marquel', 'Goku', 'Snorlax'])
@@ -31,7 +34,10 @@ test('create new account', () => {
         name: 'test',
         dateofbirth: '2/22/2222',
         email: 'root@cs.odu.edu',
-        language: 'English'
+        language: ['English', 'Spanish'],
+        country: 'usa',
+        bio: 'I am groot',
+        profilepicture: 'https://res.cloudinary.com/teepublic/image/private/s--rh264MCI--/t_Preview/b_rgb:484849,c_limit,f_jpg,h_630,q_90,w_630/v1517893785/production/designs/2341977_3.jpg'
     });
 })
 
