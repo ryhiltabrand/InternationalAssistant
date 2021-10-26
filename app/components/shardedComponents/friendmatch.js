19c5b3840d94d325b31836101265e0c97d772637
cs_nshah001 <nsha001@odu.edu>
2021-10-26 03:43:11 -0400

    Added Location and Langauge Friend Matcher Algs

--- app/components/shardedComponents/friendmatch.js
+++ app/components/shardedComponents/friendmatch.js
@@ -8,17 +8,82 @@ This is gonna be test for that timer error if node modules needs to be deleted u
 
 
 export default function FriendMatcher(){
+    var Matcher=[]
     const Loc= async () => {
-        const usersRef= firebase.firestore().collection('users');
-        const usersLoc= await usersRef.where("country", '==', 'USA').get();
-        if(usersLoc.empty){
-            console.log('No matching documents.');
-            return;
-        }
-        usersLoc.forEach(doc => {
-            var uid= doc.get("UID")
-            console.log(doc.id, '=>', uid);
-        });
+        counter=0
+        const usersRef= firebase.firestore().collection('users').where("UID", "==", firebase.auth().currentUser.uid).get();
+        (await usersRef).forEach((doc) => {
+         var location = doc.get('country')
+         var uid = doc.get('UID')
+         var friend = doc.get('FriendsList')
+         var friendslist = Object.keys(friend) 
+         firebase.firestore().collection("users")
+         .where("country", '==', location )
+         .get()
+         .then((snapshot) =>{
+             snapshot.docs.map((doc) => {
+                var otherUID= doc.get("UID")
+                if((friendslist.includes(otherUID)!=true && counter<2)){
+                    if(otherUID != uid){
+                        console.log("This is the way")
+                        console.log(otherUID)
+                        Matcher.push(otherUID)
+                        counter++
+                    }
+                }   
+               
+             })
+             
+         })
+        })
     }
-    Loc();
+    const Lang = async() => {
+        counter=0
+        const usersRef= firebase.firestore().collection('users').where("UID", "==", firebase.auth().currentUser.uid).get();
+        (await usersRef).forEach((doc) => {
+            var language = doc.get('language')
+            var uid = doc.get('UID')
+            var friend = doc.get('FriendsList')
+            var friendslist = Object.keys(friend) 
+            for(var i =0; i<language.length; i++){
+                if(language.length ==1){
+                    firebase.firestore().collection("users")
+                    .where("language", 'array-contains' , language[i] )
+                    .get()
+                    .then((snapshot) =>{
+                        snapshot.docs.map((doc) => {
+                            var otherUID = doc.get("UID")
+                            if(friendslist.includes(otherUID)!= true && counter<2 && Matcher.includes(otherUID) != true){
+                                if(otherUID != uid){
+                                    console.log("This is the way")
+                                    console.log(otherUID)
+                                    Matcher.push(otherUID)
+                                    counter++;
+                                }
+                            }   
+                        })
+                    })
+                }
+                else if(language.length>=2){
+                    firebase.firestore().collection("users")
+                    .where("language", 'array-contains' , language[i] )
+                    .get()
+                    .then((snapshot) =>{
+                        snapshot.docs.map((doc) => {
+                            var otherUID = doc.get("UID")
+                            if(friendslist.includes(otherUID)!= true && counter<2 && Matcher.includes(otherUID) != true){
+                                if(otherUID != uid){
+                                    console.log("This is the way")
+                                    console.log(otherUID)
+                                    Matcher.push(otherUID)
+                                    counter++;
+                                }
+                            }   
+                        })
+                    })
+                }
+            }
+        })
+    }
+    //Make into all one function since async is causing problems.
 }
