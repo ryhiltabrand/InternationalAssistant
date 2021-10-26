924fa3acc9101377dedd1e2271a503c48fee452c
leeshi lin <lizhilin@leeshideMacBook-Pro.local>
2021-10-26 11:58:37 -0400

    boolean instead of array

--- app/components/shardedComponents/Addfriends.js
+++ app/components/shardedComponents/Addfriends.js
@@ -8,14 +8,11 @@ export default function updatefriends(){
         
         const Uid = firebase.auth().currentUser.uid
         const friendlist = firebase.firestore().collection('users').where("UID", "==", firebase.auth().currentUser.uid).get();
+        // const friendlist = firebase.firestore().collection('users').where("UID", "==", firebase.auth().currentUser.uid).get();
         console.log(Uid)
         friendlist.then((querySnapshot) => {querySnapshot.forEach((doc) => {doc.ref.update(
-            {
-                FriendsList: firebase.firestore.FieldValue.arrayUnion({
-                  "bV999QML9PeO3tobe7Ca":true
-                })
-              } 
-        )})})
+            {"FriendsList.yVHnHIqhsOYbVbWOM6TbjpU4N3x1":true}
+            )})})
     }
     friendadd()
 
