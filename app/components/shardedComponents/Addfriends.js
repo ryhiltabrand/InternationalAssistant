4d6d408ca10c2dd3bc4da41bceb66ae4979e36d6
leeshi lin <lizhilin@leeshideMBP.fios-router.home>
2021-10-26 17:19:50 -0400

    add to friendlist on both sizes of users

--- app/components/shardedComponents/Addfriends.js
+++ app/components/shardedComponents/Addfriends.js
@@ -1,20 +1,39 @@
 import firebase from 'firebase'
 
 
-export default function updatefriends(){
+export default function updatefriends(FriendsUID){
 
     const friendadd = async() =>{
 
-        
+        // get current users uid
         const Uid = firebase.auth().currentUser.uid
-        const friendlist = firebase.firestore().collection('users').where("UID", "==", firebase.auth().currentUser.uid).get();
-        // const friendlist = firebase.firestore().collection('users').where("UID", "==", firebase.auth().currentUser.uid).get();
-        console.log(Uid)
-        friendlist.then((querySnapshot) => {querySnapshot.forEach((doc) => {doc.ref.update(
-            {"FriendsList.yVHnHIqhsOYbVbWOM6TbjpU4N3x1":true}
-            )})})
+
+        // define the current users 
+        const currentUID = firebase.firestore().collection('users').where(
+            "UID", "==", firebase.auth().currentUser.uid).get();
+           
+        // add the other users uid to current users friendlist
+        currentUID.then((querySnapshot) => 
+
+        {querySnapshot.forEach((doc) => {doc.ref.update(
+
+            {[`FriendsList.${FriendsUID}`]:true})
+        })
+    })
+    
+    // Add current users UID to other users friendlist too
+        const otherUserUID = firebase.firestore().collection('users').where(
+        "UID", "==", FriendsUID).get();
+
+        otherUserUID.then((querySnapshot) => 
+
+        {querySnapshot.forEach((doc) => {doc.ref.update(
+
+            {[`FriendsList.${Uid}`]:true})
+        })
+    })
+
     }
+
     friendadd()
-
-
 }
