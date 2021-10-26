e4421c31d8c726b68bb1ed652e7e547c1d2d3a4f
leeshi lin <lizhilin@leeshidembp.lan>
2021-10-26 11:23:15 -0400

    locate users uid and update

--- app/components/shardedComponents/Addfriends.js
+++ app/components/shardedComponents/Addfriends.js
@@ -6,17 +6,16 @@ export default function updatefriends(){
     const friendadd = async() =>{
 
         
-       // get the current user's uid
         const Uid = firebase.auth().currentUser.uid
-        const friendlist = firebase.firestore().collection('users').doc(Uid)
-        // const friendlist = firebase.firestore().collection('users').where("UID", "==", firebase.auth().currentUser.uid).get();
+        const friendlist = firebase.firestore().collection('users').where("UID", "==", firebase.auth().currentUser.uid).get();
         console.log(Uid)
-        const addfriend = await friendlist.update({
-            "FriendsList.yVHnHIqhsOYbVbWOM6TbjpU4N3x1":true
-        })
-    
-
-        
+        friendlist.then((querySnapshot) => {querySnapshot.forEach((doc) => {doc.ref.update(
+            {
+                FriendsList: firebase.firestore.FieldValue.arrayUnion({
+                  "bV999QML9PeO3tobe7Ca":true
+                })
+              } 
+        )})})
     }
     friendadd()
 
