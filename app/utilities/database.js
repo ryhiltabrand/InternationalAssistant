a4c2da0653743accb9cf70441b462db28c2a013d
cs_mresu001 <mresu001@odu.edu>
2021-10-27 23:00:12 -0400

    Convert Firebase 9 to 8

--- app/utilities/database.js
+++ app/utilities/database.js
@@ -1,18 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, setDoc, collection, getDoc, getDocs, addDoc, query, where} from "firebase/firestore";
+//import { initializeApp } from "firebase/app";
+import firebaseCred from './firebaseAuth.js';
+//import { getFirestore, setDoc, collection, getDoc, getDocs, addDoc, query, where} from "firebase/firestore";
+const firebase = require("firebase");
+require("firebase/firestore");
 
 // import {credentials} from './firebaseAuth.js';
-import {credentials} from './firebaseAuth.js';
 
-const firebaseApp = initializeApp(credentials);
-const db = getFirestore();
+//const firebaseApp = initializeApp(credentials);
+var db = firebaseCred.firestore();
 
 //User Collection
 
 export class UsersCollection {
     #userFieldEntries = {};
 
-//Accessor and Mutator for 'users' collection
+    //Accessor and Mutator for 'users' collection
 
     setName(name) {
         this.#userFieldEntries.name = name
@@ -26,7 +28,7 @@ export class UsersCollection {
         this.#userFieldEntries.email = email
     }
 
-    setLanguage(language) { 
+    setLanguage(language) {
         this.#userFieldEntries.language = language
     }
 
@@ -47,8 +49,10 @@ export class UsersCollection {
     }
 
 
-//Creates a User Document with given fields from the userFieldEntries object
+    //Creates a User Document with given fields from the userFieldEntries object
     async createUserAccountInformation() {
+
+        db.collection("users").doc(this.#userFieldEntries.UID).set(this.#userFieldEntries)
 
         //doc = getAccountInformation(this.#userFieldEntries.UID)
         //console.error(doc)
@@ -72,45 +76,68 @@ export class UsersCollection {
         //     }
         // }
 
-        try {
-            await addDoc(collection(db, 'users'), this.#userFieldEntries)
-        } catch (err) {
-            console.error("Error adding Users document with UID ", this.#userFieldEntries.UID, err)
-        }
 
+        // Add user entry with doc ID set to random
+        // try {
+        //     await addDoc(collection(db, 'users'), this.#userFieldEntries)
+        // } catch (err) {
+        //     console.error("Error adding Users document with UID ", this.#userFieldEntries.UID, err)
+        // }
 
+        // Add user entry with doc ID set to UID
         // try {
         //     await setDoc(doc(db, "users", this.#userFieldEntries.UID), this.#userFieldEntries);
         //     console.log("Users document written with ID: ", this.#userFieldEntries.UID);
         // } catch (e) {
         //     console.error("Error adding Users document with UID ", e);
         // }
+
+        // Firebase 8 Implementation for setting doc ID by user's UID
+
     }
 
-//Retrieves User Information from document based on UID
+    //Retrieves User Information from document based on UID
     async getAccountInformation(UID) {
+
+        docRef = db.collection("users").doc(UID);
+        //return docSnap.then;
+        return docRef.get().then(function(doc) {
+            if (doc.exists) {
+                //console.log(doc.data())
+                stuff = doc.data();
+                return stuff;
+            }
+            // else {
+            //     return Promise.reject("No such UserAccountInformation document for ", UID);
+            //}
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
+
+/*         search document by querying UID field
         const UIDQuery = query(collection(db, "users"), where("UID", "==", UID));
         const querySnapshot = await getDocs(UIDQuery);
         console.log(querySnapshot.docs[0].id)
         return querySnapshot.docs[0].id;
-        // const docRef = "cm0IF5KopLgxnJTUtfi403kJuMl2"
-        // const docSnap = await getDoc(docRef);
+        const docRef = UID
+        const docSnap = await getDoc(docRef);
 
-        // if (docSnap.exists()) {
-        //     console.log("Document data:", docSnap.data());
-        //     //Returns user object
-        //     return docSnap.data();
-        // } else {
-        //     // doc.data() will be undefined in this case
-        //     return Promise.reject("No such UserAccountInformation document for ", email);
-        //     //console.log("No such UserAccountInformation document for ", email);
-        // }
+        if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+            //Returns user object
+            return docSnap.data();
+        } else {
+            // doc.data() will be undefined in this case
+            return Promise.reject("No such UserAccountInformation document for ", email);
+            //console.log("No such UserAccountInformation document for ", email);
+        }
+ */
     }
 
     // async deleteAccountInformation(email) {
     //         await deleteDoc(doc(email));
     //     }
-    
+
 }
 
 //Other Collection
--- app/utilities/firebaseAuth.js
+++ app/utilities/firebaseAuth.js
@@ -1,18 +1,17 @@
+import { initializeApp } from "firebase/app";
+
 // Initialize Firebase
 export const credentials = {
     //Information can be define in CI varibles. Then sed in by the gitlab runner
-    apiKey: "$APIKEY",
-  
-    authDomain: "$AUTHDOMAIN",
-  
-    projectId: "$PROJECTID",
-  
-    storageBucket: "$STORAGEBUCKET",
-  
-    messagingSenderId: "$MESSAGINGSENDERID",
-  
-    appId: "$APPID",
-  
-    measurementId: "$MEASUREMENTID"
+    apiKey: "AIzaSyBURNkibRwSyghNlodMToYyjWWUdctdtto",
+    authDomain: "internationalassistant-2145f.firebaseapp.com",
+    projectId: "internationalassistant-2145f",
+    storageBucket: "internationalassistant-2145f.appspot.com",
+    messagingSenderId: "669783546610",
+    appId: "1:669783546610:web:21975730b89aa3d21d4e6b",
+    measurementId: "G-SQY6PDPBMJ"
   
   };
+
+ const firebaseCred = initializeApp(credentials);
+ export default firebaseCred;
