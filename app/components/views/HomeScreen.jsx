370c2c321cf85ee54e31b7b3f25cf2444ca6f606
leeshi lin <lizhilin@leeshideMacBook-Pro.local>
2021-11-08 19:06:21 -0500

    addfriend merge

--- app/components/views/HomeScreen.jsx
+++ app/components/views/HomeScreen.jsx
@@ -12,10 +12,6 @@ const HomeScreen = () => {
         onPress={() => console.log("hello")}
         title="you are home"
       />
-      <Button
-        onPress={()=> updatefriends("cm0IF5KopLgxnJTUtfi403kJuMl2")}
-        title= "fake"/>
-        
     </View>
   )
 }
