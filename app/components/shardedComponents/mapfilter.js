16b21568dca2e3caaafb5c0922d647aa27bb9881
leeshi lin <lizhilin@leeshidembp.lan>
2021-11-29 20:10:58 -0500

    new mapsort ready to merge

--- app/components/shardedComponents/mapfilter.js
+++ app/components/shardedComponents/mapfilter.js
@@ -2,16 +2,19 @@ import firebase from 'firebase';
 
 
 export default function filtercat(type){
-    
+
     var locList = []
 
     const filterbytype = async() =>{
 
-        
-
-        const snapshot = await firebase.firestore().collection('Locations').where(
+        // will filter by the type of location, and then sort by rating
+        var snapshot = await firebase.firestore().collection('Locations').where(
             "category", "==", type).orderBy('rating','desc').get();
         
+        // this is for All category, it will display all locations
+        if (type == 'All'){
+            var snapshot = await firebase.firestore().collection('Locations').orderBy('rating','desc').get()
+        }
         if (snapshot.empty){
             console.log('No matching documents');
             return;
