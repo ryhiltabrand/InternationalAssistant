7688e809af7301c72faaa4bcad235f6bb3a8d8d2
cs_nshah001 <nsha001@odu.edu>
2021-12-07 06:32:08 -0500

    Added Upvote and DownVote and cleaned QandA

--- app/components/views/QandA/othersQuestion.jsx
+++ app/components/views/QandA/othersQuestion.jsx
@@ -8,10 +8,12 @@ import {
   StyleSheet,
   Image,
   FlatList,
+  TouchableOpacity,
 } from "react-native";
-import React, { TouchableOpacity, useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import ApplyForRequest from "./../../shardedComponents/Help/ApplyForRequest";
 import firebase from "firebase";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function OthersQuestion({ route, navigation }) {
   /* 2. Get the param */
@@ -19,11 +21,81 @@ export default function OthersQuestion({ route, navigation }) {
 
   const [replys, setreplys] = useState([]);
   const [answers, setanswers] = useState(null);
-
+  const [likes, setlikes] = useState(null);
+  const [donwvotes, setdownvotes] = useState(null);
+  const [total, settotal] = useState(null)
+  //const [text, onChangeText]
   useEffect(() => {
     puller();
   },[]);
 
+  var Upvoter = async(ID,Rater) =>{
+    var RaterA = Object.keys(Rater)
+    var RaterV = Object.values(Rater)
+    var Likes = Object.keys(Rater).reduce((o,key) =>{
+      Rater[key]  === "Like" && (o[key] = Rater[key])
+      return o;
+    }, {});
+    if(Likes[firebase.auth().currentUser.uid] !== "Like"){
+      const updater = await firebase.firestore().collection("Questions and Answers")
+      .doc(DocID)
+      .collection("Answers")
+      .doc(ID)
+      .update({
+        [`Rater.${firebase.auth().currentUser.uid}`] : 'Like'
+      })
+      const checker = await firebase.firestore().collection("Questions and Answers")
+      .doc(DocID)
+      .collection("Answers")
+      .doc(ID)
+      .get()
+      const V2= checker.data().Rater;
+      var RaterA = Object.keys(V2)
+      var RaterV = Object.values(V2)
+      var Likes = Object.keys(V2).reduce((o,key) =>{
+      V2[key]  === "Like" && (o[key] = V2[key])
+      return o;
+    }, {});
+      setlikes(Object.keys(Likes).length)
+      settotal((likes-donwvotes))
+      puller();
+      console.log(total)
+    }
+
+  }
+  var Downvoter = async(ID,Rater) =>{
+    var RaterA = Object.keys(Rater)
+    var RaterV = Object.values(Rater)
+    var Dislikes = Object.keys(Rater).reduce((o,key) =>{
+      Rater[key]  === "Dislike" && (o[key] = Rater[key])
+      return o;
+    }, {});
+    if(Dislikes[firebase.auth().currentUser.uid] !== "Dislike"){
+      const updater = await firebase.firestore().collection("Questions and Answers")
+      .doc(DocID)
+      .collection("Answers")
+      .doc(ID)
+      .update({
+        [`Rater.${firebase.auth().currentUser.uid}`] : 'Dislike'
+      })
+      const checker = await firebase.firestore().collection("Questions and Answers")
+      .doc(DocID)
+      .collection("Answers")
+      .doc(ID)
+      .get()
+      const V2= checker.data().Rater;
+      var RaterA = Object.keys(V2)
+      var RaterV = Object.values(V2)
+      var Dislikes = Object.keys(V2).reduce((o,key) =>{
+      V2[key]  === "Dislike" && (o[key] = V2[key])
+      return o;
+    }, {});
+      setdownvotes(Object.keys(Dislikes).length)
+      settotal((likes-donwvotes))
+      puller();
+      console.log(total)
+    }
+  }
 
   async function puller() {
     setreplys([])
@@ -39,9 +111,22 @@ export default function OthersQuestion({ route, navigation }) {
       var pic = doc.data().Pic
       var text = doc.data().Text
       var id = doc.id
-      var like = doc.data().Like
-      var dislike = doc.data().Dislike
+      var Rater = doc.data().Rater;
       var size = replyref.size
+      var RaterA = Object.keys(Rater)
+      var RaterV = Object.values(Rater)
+      var Likes = Object.keys(Rater).reduce((o,key) =>{
+      Rater[key]  === "Like" && (o[key] = Rater[key])
+      return o;
+      }, {});
+      var RaterA = Object.keys(Rater)
+      var RaterV = Object.values(Rater)
+      var Dislikes = Object.keys(Rater).reduce((o,key) =>{
+        Rater[key]  === "Dislike" && (o[key] = Rater[key])
+        return o;
+      }, {});
+
+      settotal((Object.keys(Likes).length) - (Object.keys(Dislikes).length))
       
       let reply = {
         Name: name,
@@ -50,6 +135,7 @@ export default function OthersQuestion({ route, navigation }) {
         ID: id,
         Dislike: dislike,
         Like: like,
+        Rater: Rater,
       }
       setanswers(size)
       setreplys((replys) => [...replys, reply]);
@@ -92,8 +178,20 @@ export default function OthersQuestion({ route, navigation }) {
               <View style={styles.firstLine}>
                 <Image style={styles.image} source={{ uri: item.Pic }} />
                 <Text style={styles.name}>{item.Name}</Text>
+                <TouchableOpacity 
+                style={styles.name}
+                onPress={() => Upvoter(item.ID,item.Rater)}>
+                  <FontAwesome name="thumbs-up" size={22} color='black'/>
+                </TouchableOpacity>
+                <View style={{borderColor:'black', alignItems:'center', paddingTop:11, marginLeft:10,}}>
+                <Text style={{fontSize:22}} onChange={settotal}>{total}</Text>
+                </View>
+                <TouchableOpacity 
+                style={styles.name}
+                onPress={() => Downvoter(item.ID,item.Rater)}>
+                  <FontAwesome name="thumbs-down" size={22} color='black'/>
+                </TouchableOpacity>
               </View>
-
               <View style={styles.secondLine}>
                 <Text style={styles.request}>{item.Text}</Text>
               </View>
